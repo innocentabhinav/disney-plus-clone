71c719affd2d4150a9b0199a1184b85500c7a483
innocentabhinav <abhinavprakashak47@gmail.com>
2022-12-26 15:39:43 +0530

    header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <Home />
+    </div>
+  );
+}
+
+export default App;
